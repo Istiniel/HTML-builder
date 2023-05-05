a9912f6209bda27ed5f776459a359ff356f455c8
Istiniel <eclipsio@mail.ru>
2023-05-05 12:29:36 +0300

    fix: fix task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,13 +8,17 @@ fs.readdir(
     if (error) throw new Error('something got wrong...');
     for (const file of files) {
       if (file.isFile()) {
-        const [name, ...ext] = file.name.split('.');
+        const fileInfo = file.name.split('.');
         fs.stat(
           path.join(__dirname, 'secret-folder', file.name),
           (error, stats) => {
             if (error) throw new Error('something got wrong...');
             const weight = stats.size;
-            console.log(`${name} - ${ext.at(-1)} - ${weight}`);
+            console.log(
+              `${fileInfo
+                .slice(0, fileInfo.length - 1)
+                .join('.')} - ${fileInfo.at(-1)} - ${weight}`
+            );
           }
         );
       }
