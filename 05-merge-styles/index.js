0e86922051da186b7b56520788c7e851a75eb42d
Istiniel <eclipsio@mail.ru>
2023-05-05 13:05:04 +0300

    fix: fix 05 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,42 +2,46 @@ const fs = require('fs');
 const path = require('path');
 
 fs.rm(
-  path.resolve(__dirname, './project-dist', 'bundle.css'),
+  path.resolve(__dirname, './project-dist'),
   { recursive: true, force: true },
   (error) => {
-    if (error) throw new Error('something got wrong...');
+    if (error) throw new Error('something got wrong1...');
 
-    fs.writeFile(
-      path.resolve(__dirname, './project-dist', 'bundle.css'),
-      '',
-      (error) => {
-        if (error) throw new Error('something got wrong...');
+    fs.mkdir(path.resolve(__dirname, './project-dist'), (error) => {
+      if (error) throw new Error('something got wrong2...');
 
-        const output = fs.createWriteStream(
-          path.resolve(__dirname, './project-dist', 'bundle.css')
-        );
+      fs.writeFile(
+        path.resolve(__dirname, './project-dist', 'bundle.css'),
+        '',
+        (error) => {
+          if (error) throw new Error('something got wrong3...');
 
-        fs.readdir(
-          path.resolve(__dirname, './styles'),
-          { withFileTypes: true },
-          (error, files) => {
-            if (error) throw new Error('something got wrong...');
-            for (const file of files) {
-              const [, ...ext] = file.name.split('.');
+          const output = fs.createWriteStream(
+            path.resolve(__dirname, './project-dist', 'bundle.css')
+          );
 
-              if (file.isFile() && ext.at(-1) === 'css') {
-                const input = fs.createReadStream(
-                  path.resolve(__dirname, './styles', `${file.name}`),
-                  {
-                    encoding: 'utf-8',
-                  }
-                );
-                input.pipe(output);
+          fs.readdir(
+            path.resolve(__dirname, './styles'),
+            { withFileTypes: true },
+            (error, files) => {
+              if (error) throw new Error('something got wrong3...');
+              for (const file of files) {
+                const [, ...ext] = file.name.split('.');
+
+                if (file.isFile() && ext.at(-1) === 'css') {
+                  const input = fs.createReadStream(
+                    path.resolve(__dirname, './styles', `${file.name}`),
+                    {
+                      encoding: 'utf-8',
+                    }
+                  );
+                  input.pipe(output);
+                }
               }
             }
-          }
-        );
-      }
-    );
+          );
+        }
+      );
+    });
   }
 );
